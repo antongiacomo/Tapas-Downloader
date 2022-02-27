de0e7e48528b3b5785d55339f1823a5c5e372ab3
Antongiacomo Polimeno <antongiacomo.polimeno@unimi.it>
2022-02-27 22:09:31 +0100

    Improved images preview

--- popup.js
+++ popup.js
@@ -1,46 +1,78 @@
 // Initialize butotn with users's prefered color
 let changeColor = document.getElementById("changeColor");
 let img = document.getElementById("button_img");
-chrome.storage.sync.get("color", ({ color }) => {
-    changeColor.style.backgroundColor = color;
-});
-chrome.storage.sync.get("url", ({ url }) => {
-    img.src = url;
+var block = document.getElementById("imgs");
+
+chrome.tabs.query({ active: true, currentWindow: true }).then(([tab]) => {
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: loadImages,
+    },
+    (urls) => {
+      for (const frameResult of urls) {
+        urls = frameResult.result;
+        var imgs = urls.map(function (URL) {
+          var img = document.createElement("img");
+          img.src = URL;
+          block.appendChild(img);
+          return img;
+        });
+      }
+    }
+  );
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: setPageBackgroundColor,
-    });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
 });
 
 // The body of this function will be execuetd as a content script inside the
 // current page
-function setPageBackgroundColor() {
-    function isValidHttpUrl(string) {
-        let url;
+function loadImages() {
+  function isValidHttpUrl(string) {
+    let url;
 
-        try {
-            url = new URL(string);
-        } catch (_) {
-            return false;
-        }
-
-        return url.protocol === "http:" || url.protocol === "https:";
+    try {
+      url = new URL(string);
+    } catch (_) {
+      return false;
     }
 
-    chrome.storage.sync.get("color", ({ color }) => {
-        //document.body.style.backgroundColor = color;
-        imgs = document.getElementsByClassName("content__img");
-        urls = [].map.call(imgs, (img) => img.src);
-        urls = urls.filter((url) => isValidHttpUrl(url));
+    return url.protocol === "http:" || url.protocol === "https:";
+  }
 
-        console.log(urls);
-        chrome.storage.sync.set({ urls });
-        chrome.runtime.sendMessage(urls);
-    });
+  imgs = document.getElementsByClassName("content__img");
+  urls = [].map.call(imgs, (img) => img.src);
+  urls = urls.filter((url) => isValidHttpUrl(url));
+  chrome.storage.local.set({ urls: urls });
+
+  return urls;
+}
+
+function setPageBackgroundColor() {
+  function isValidHttpUrl(string) {
+    let url;
+
+    try {
+      url = new URL(string);
+    } catch (_) {
+      return false;
+    }
+
+    return url.protocol === "http:" || url.protocol === "https:";
+  }
+
+  imgs = document.getElementsByClassName("content__img");
+  urls = [].map.call(imgs, (img) => img.src);
+  urls = urls.filter((url) => isValidHttpUrl(url));
+
+  console.log(urls);
+  chrome.storage.sync.set({ urls });
+  chrome.runtime.sendMessage(urls);
 }
